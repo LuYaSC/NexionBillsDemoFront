a93ea32832def0a149c972a57a45ea427a3752c0
Luis Jose Jimenez Peña <luisjimpe93@gmail.com>
2021-11-21 22:50:19 -0400

    add url to users

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
   url: {
-    services: 'https://localhost:44346/api/Services/',
-    states: 'https://localhost:44346/api/States/',
-    coins: 'https://localhost:44346/api/Currencies/',
+    services: 'https://localhost:5000/api/Services/',
+    states: 'https://localhost:5000/api/States/',
+    coins: 'https://localhost:5000/api/Currencies/',
+    users: 'https://localhost:5000/api/Users/',
   }
 };
